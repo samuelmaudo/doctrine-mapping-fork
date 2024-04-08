d97a1ec4482554622a6a8822ffa1c702547a314e
Samuel Maudo <samuel.maudo@exoticca.com>
2024-04-08 09:31:22 +0200

    Fix unit tests

--- tests/CustomIdGenerator/Class/ExistingIdGenerator.php
+++ tests/CustomIdGenerator/Class/ExistingIdGenerator.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Hereldar\DoctrineMapping\Tests\CustomIdGenerator\Class;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Id\AbstractIdGenerator;
 
-final class ExistingIdGenerator extends AbstractIdGenerator {}
+final class ExistingIdGenerator extends AbstractIdGenerator
+{
+    /**
+     * @param object|null $entity
+     */
+    public function generateId(EntityManagerInterface $em, $entity): array
+    {
+        return [];
+    }
+}
